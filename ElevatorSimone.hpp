cc003012cf8e71f583e2a7fa0f51eac35bd483f6
dj <djthetromboneplayer@gmail.com>
2015-09-29 14:29:39 -0700

    Added the actual file

---
+++ ElevatorSimone.hpp
@@ -0,0 +1,30 @@
+class ElevatorSimone {
+     ElevatorSimone::ElevatorSimone(){
+	 idlePosition = (maxHeight + minHeight)/2;
+	 currentLevel = idlePosition;
+	 currentWeight = 0;
+	 currentDirection = 0; // -1: down 0: not moving 1: up	 
+	 }
+  public:
+	int currentLevel, currentDirection,
+    int runElevator(){
+		input();
+		elevatorAlgorithm();
+		
+	}
+	int stopElevator(){
+		
+		
+	}
+	
+	private:
+	int currentWeight, maxHeight, minHeight, idlePosition;
+	queue floorsPressed;
+	int[] input(){
+		std::cout << "Enter Floors: " << std::endl;
+		string line;
+		line.getline();
+		return a;
+	}
+	
+};
