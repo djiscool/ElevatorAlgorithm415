7bba8ece516a787903048b0064642929b93bedf0
dj <djthetromboneplayer@gmail.com>
2015-10-28 18:18:22 -0700

    Added some extra checks, execute Queue empties both queues
    -It still does not ask where the callee wants to go.
    -executeQueue will now call itself to finish any requests that were
    going the opposite direction
    - Added a check to see if already at floor.
    - Added loop to run elevator multiple times
    - Elevator now prints when elevator goes IDLE, and which floor its on
    before and after input. Also added final resting place output.

--- cs415_elevator.cpp
+++ cs415_elevator.cpp
@@ -1,5 +1,5 @@
 #include "cs415_elevator.hpp"
-
+//constructor
 cs415_elevator::cs415_elevator()
 {
 	currentFloor = 0;
@@ -12,33 +12,30 @@ cs415_elevator::cs415_elevator()
 cs415_elevator::~cs415_elevator()
 {
 }
-
+//takes input and queues up elevator calls
 void cs415_elevator::call(int input_direction)
 {	
 	int whichQueue = 0;
     if( !(input_direction >= -1 && input_direction <= 1) ) {
         std::cout << "A proper direction was not given." << std::endl;
     } else {
-	if( direction == IDLE){
-		direction = input_direction;		
-	}
-	if(input_direction != direction){
-		std::cout << "Elevator is going opposite direction, call queued for later" << std::endl;
-	}
-	
-        if( input_direction == DOWN ){
+		if( direction == IDLE){
+			direction = input_direction;		
+		}
+		if(input_direction != direction){
+			std::cout << "Elevator is going opposite direction, call queued for later" << std::endl;
+		}
+		if( input_direction == DOWN ){
             std::cout << "Elevator called to head down." << std::endl;
-            //move_down();
-		whichQueue = DOWN;
-	}
+			whichQueue = DOWN;
+		}
         else if( input_direction == UP ){
             std::cout << "Elevator called to head up." << std::endl;
-       	    //move_up();
-		whichQueue = UP;
-	}
-        else if( input_direction == IDLE )
+			whichQueue = UP;
+		}
+        else if( input_direction == IDLE ){
             std::cout << "Elevator is idle." << std::endl;
-    
+		}
 	if(whichQueue == UP){
 		minQueue.push(get_req_floor());
 	}else{
@@ -47,35 +44,45 @@ void cs415_elevator::call(int input_direction)
 	
 }
 }
+//moves the elevator to queued levels, will only stop if direction desired is the same as the elevators direction.
+// calls itself to go the other direction when complete if not empty.
+//sets to IDLE when complete
 void cs415_elevator::executeQueue(){
 	//go through queue to each floor
-	
+	if(maxQueue.empty() && minQueue.empty()){ // a little bit of recursion in my life
+		return;
+	}
+	std::cout << "Elevator is at: " << currentFloor << std::endl;
 	int flr;
 	if(direction == UP){
+		while(!minQueue.empty()){
+			flr = minQueue.top();
+			minQueue.pop();
+			currentFloor = flr;
 
-	while(!minQueue.empty()){
-		flr = minQueue.top();
-		minQueue.pop();
-		currentFloor = flr;
-
-		std::cout << "waiting for move ......." << std::endl;
-		std::cout << "moved to " << currentFloor << std::endl;
-
-	}	
-
-	}else{
-
-
-	while(!maxQueue.empty()){
-		flr = maxQueue.top();
-		maxQueue.pop();
-		currentFloor = flr;
-
-		std::cout << "waiting for move ......." << std::endl;
-		std::cout << "moved to " << currentFloor << std::endl;
-
+			std::cout << "waiting for move ......." << std::endl;
+			std::cout << "moved to " << currentFloor << std::endl;
+		}	
 	}
-	}	
+	else if(direction == DOWN){
+		while(!maxQueue.empty()){
+			flr = maxQueue.top();
+			maxQueue.pop();
+			currentFloor = flr;
+
+			std::cout << "waiting for move ......." << std::endl;
+			std::cout << "moved to " << currentFloor << std::endl;
+		}
+	}
+	if(direction == UP){
+		direction = DOWN;
+	}
+	else{
+		direction = UP;
+	}
+	executeQueue();
+	direction = IDLE;
+	std::cout << "Elevator is IDLE" << std::endl;
 }
 
 void cs415_elevator::setMax(int MAX){
