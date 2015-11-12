5e161e79098f9fcf83c62a5cd17e99727a736861
dj <djthetromboneplayer@gmail.com>
2015-11-11 18:30:56 -0800

    Fixed Infinite loop
    Added check for when elevator wanting to go a certain direction, but the
    elevator is already past. i.e. request to go to floor 5, and he/she
    wants to go down, but elevator is at 1 and is going up. 5 will be added
    to queue to go up.
    This may create picking up a person and going the wrong way, further
    thinking required.

--- cs415_elevator.hpp
+++ cs415_elevator.hpp
@@ -6,23 +6,24 @@
  ***********************/
 #include <iostream>
 #include <queue>
+#include <unistd.h>
 class cs415_elevator
 {
 public:
     //  Constructor & Desconstructor
     cs415_elevator();
     ~cs415_elevator();
-    
+
     void call(int direction);
     void move_up();
     void move_down();
-    
+
     int get_current_floor();
     void set_current_floor(int floor);
-    
+
     int get_req_floor();
     void set_req_floor(int floor);
-    
+
     void setMax(int MAX);
     void executeQueue();
 private:
@@ -33,7 +34,7 @@ private:
             return l > r;
         }
     };
-    
+
     int maxFloors;
     int currentFloor;
     int floorReq;
@@ -43,7 +44,8 @@ private:
     std::queue<int> floorQueue;
     std::priority_queue<int,std::vector<int>, compare > minQueue;
     enum direction { DOWN = -1, IDLE = 0, UP = 1 };
-    
-    
+
+
 };
 #endif
+
